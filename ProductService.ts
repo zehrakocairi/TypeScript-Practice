11c1867aabf89ce0060e98795342924c4355f3e0
zehra <zehrakocairi@gmail.com>
2024-03-16 21:56:42 +0100

    some practice

---
+++ ProductService.ts
@@ -0,0 +1,17 @@
+import { IProductService } from "./IProductService";
+import { Product } from "./Product";
+
+class ProductService implements IProductService{
+ getById(id: number): Product {
+     
+ }
+ getProducts(): Product[] {
+     
+ }
+ saveProduct(product: Product): void {
+     
+ }
+ deleteProduct(product: Product): void {
+     
+ }
+}
---
+++ SimpleDataSource.ts
@@ -0,0 +1,19 @@
+import { Product } from "./Product";
+
+export class SimpleDataSource {
+    private products:Array<Product>;
+
+    constructor (){
+        this.products= new Array<Product>(
+            new Product (1,"Samsung S5","Phone",200),
+            new Product (2,"Samsung S6","Phone",300),
+            new Product (3,"Samsung S7","Phone",400),
+            new Product (4,"Samsung S8","Phone",500),
+            );
+    }
+
+    getProducts(): Product[]{
+      return this.products
+    }
+    
+}
